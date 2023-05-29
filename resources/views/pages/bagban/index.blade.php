676d21f031f4c297cb730e3d239c0ac8209831e9
yessentay01 <ye.kurmanbay@iitu.edu.kz>
2023-05-30 02:30:56 +0600

    Add functions

--- resources/views/pages/bagban/index.blade.php
+++ resources/views/pages/bagban/index.blade.php
@@ -51,15 +51,7 @@
 
 
     <script>
-        var config = {
-            apiKey: "AIzaSyDDWMOffTbHcfw2o6Zc0Dwq0fmmHbGQaiA",
-            authDomain: "bagban-c467d.firebaseapp.com",
-            databaseURL: "https://bagban-c467d-default-rtdb.firebaseio.com",
-            projectId: "bagban-c467d",
-            storageBucket: "bagban-c467d.appspot.com",
-            messagingSenderId: "964965214585",
-            appId: "1:964965214585:web:8d114b14f63d0b96b727a8"
-        };
+
 
         firebase.initializeApp(config);
 
